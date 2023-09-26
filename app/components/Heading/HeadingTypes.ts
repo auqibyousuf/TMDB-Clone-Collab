5ace069a0ec3f7ad023e54c7eaed03840ec408ca
auqibyousuf <auqib.yousuf@axelerant.com>
2023-09-26 15:54:15 +0530

    Heading component with variants and story
    Signed-off-by: auqibyousuf <auqib.yousuf@axelerant.com>

---
+++ app/components/Heading/HeadingTypes.ts
@@ -0,0 +1,6 @@
+export type HeadingTypes = {
+  text: string;
+  variant: "20" | "22" | "24" | "32" | "36" | "48";
+  headingElement: "1" | "2" | "3" | "4" | "5" | "6";
+  extraClasses?: string;
+};
