84e79bd80e546ab25053697ca8ebf32ab82793d8
Auqib Yousuf <yauqib@gmail.com>
2023-10-05 19:30:32 +0530

    small fixes to link and button component
    Signed-off-by: Auqib Yousuf <yauqib@gmail.com>

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -4,7 +4,7 @@ import classNames from 'classnames'
 
 const Button = ({ text, variant, extraClasses, onClick }: ButtonType) => {
   const buttonClasses = classNames({
-    'py-3 px-6 rounded-30 bg-linearGradient text-white px-7 py-3  w-28 h-12 flex items-center justify-center absolute right-0':
+    'py-3 px-6 rounded-30 bg-linearGradient text-white px-7 py-3  w-28 h-12 flex items-center justify-center':
       variant == 'hero-section',
   })
   return (
